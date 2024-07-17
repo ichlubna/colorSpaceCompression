2ad42cdef3901f607f64ada84df2f07b6bacbba6
ichlubna <ichlubna@fit.vutbr.cz>
2024-07-17 15:12:57 +0200

    Fixes

--- renderData.sh
+++ renderData.sh
@@ -1,6 +1,6 @@
 PROJECTS=$(ls projects/*.blend)
 DISPLAYS=("Camera Log" "Log" "sRGB")
-VIEWS_CAMERA_LOG=("CanonLog2" "CanonLog3" "V-Log V-Gamut" "S-Log2 - ITU-709" "S-Log3 - S-Gamut3" "F-Log F-Gamut" "F-Log2 F-Gamut" "D-Log D-Gamut" "ProTune Log" "N-Log" "AppleLog" "BMDFilm Gen5") 
+VIEWS_CAMERA_LOG=("CanonLog2" "CanonLog3" "V-Log V-Gamut" "S-Log2 ITU-709" "S-Log3 S-Gamut3" "F-Log F-Gamut" "F-Log2 F-Gamut" "D-Log D-Gamut" "ProTune Log" "N-Log" "Apple Log" "BMDFilm Gen5") 
 VIEWS_LOG=("AgX Log" "DaVinci Intermidiate" "Filmlight T-Log - E-Gamut")
 VIEWS_SRGB=("None" "Standard" "AgX" "AgX Kraken" "ACES" "TCAMv2" "ARRI K1S1" "RED IPP2" "OpenDRT" "JzDT" "Khronos Neutral")
 VIEWS=( "${VIEWS_CAMERA_LOG[@]}" "${VIEWS_LOG[@]}" "${VIEWS_SRGB[@]}" )
@@ -20,7 +20,8 @@ for PROJECT in "${PROJECTS[@]}"; do
         fi
         PROJECT_NAME=$(basename -- "$PROJECT")
         PROJECT_NAME="${PROJECT_NAME%.*}"
-        $($BLENDER -b $PROJECT --python render.py -x 1 -o ./renders/$PROJECT_NAME/$VIEW/ -f 1 -- "$DISPLAY" "$VIEW")
+        VIEW_NAME=$(echo "$VIEW" | tr ' ' '_')
+        $BLENDER -b $PROJECT --python render.py -x 1 -o ./renders/$PROJECT_NAME/$VIEW_NAME/ -f 1 -- "$DISPLAY" "$VIEW"
         I=$((I+1))
     done
 done
