1bbe193237eb1fbcdd63fd5eaf54241a79a62b06
Tomáš Chlubna <ichlubna@fit.vutbr.cz>
2025-11-20 10:56:01 +0100

    Update uvgMeasurement.sh

--- uvgMeasurement.sh
+++ uvgMeasurement.sh
@@ -38,7 +38,7 @@ for INPUT_FILE in $INPUT_DIR/*.yuv; do
             COMPRESSED_FILE="$PROFILE_DIR"/$CRF".266"
             DECOMPRESSED_FILE="$PROFILE_DIR"/$CRF"_dec.y4m"
 
-            $VVENC -i "$CONVERTED_FILE" -c yuv420_10 --preset fast -q $CRF -o "$COMPRESSED_FILE"
+            $VVENC -i "$CONVERTED_FILE" -c yuv420_10 --preset slow -q $CRF -o "$COMPRESSED_FILE"
             SIZE=$(stat --printf="%s" "$COMPRESSED_FILE")
             $VVDEC -b "$COMPRESSED_FILE" -o "$DECOMPRESSED_FILE"
             
